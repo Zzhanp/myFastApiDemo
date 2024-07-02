e2e3fba9b5260b69c23e50ce09125c6fde390045
Zzhan <zhangzhanpeng659@sf-express.com>
2024-07-02 14:53:06 +0800

    init

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,5 @@
 from fastapi import FastAPI
-
+from apps.urls import gamePlatform
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(gamePlatform)
